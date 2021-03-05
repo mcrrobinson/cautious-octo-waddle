68526c8b1a1cb8b15775ac77901fc58022906147
mcrrobinson <mrmcrrobinson@gmail.com>
2021-03-05 18:24:38 +0000

    [Added] - More efficient method of calculating the mandelbrot. - More compile time definitions and customisations. - Vector structure for readability purposes when it comes to iterating through the sounds. - Need to find a way not to have to stop a stream when reading the next input.

--- caucious-octo-waddle/main.cpp
+++ caucious-octo-waddle/main.cpp
@@ -1,54 +1,9 @@
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <portaudio.h>
-#include <complex> // for complex numbers
-#include <iostream>
-
-#define TABLE_SIZE (400)
-#define SAMPLE_RATE (44100)
-#define FRAMES_PER_BUFFER (64)
-#define BLOCKING_MILLISECONDS (200)
-
-#ifndef M_PI
-#define M_PI (3.14159265)
-#endif
-
-// This is the resolution
-#define WIDTH (1200)
-#define HEIGHT (1200)
+#include "main.h"
 
 using namespace cv;
 
-typedef struct paTestData
-{
-    float pitch;
-    unsigned long n;
-};
-
-typedef struct imageStuff
-{
-    Mat image;
-    int columnValue[WIDTH]{};
-};
-
-struct Timer
-{
-    std::chrono::time_point<std::chrono::steady_clock> start, end;
-
-    Timer()
-    {
-        start = std::chrono::high_resolution_clock::now();
-    }
-    ~Timer()
-    {
-        end = std::chrono::high_resolution_clock::now();
-        std::cout << "Time taken: " << (std::chrono::duration<float>(end - start)).count() * 1000.f << "ms\n";
-    }
-};
-
 static int patestCallback(const void* inputBuffer, void* outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userData) {
-    paTestData* data = (paTestData*)userData;
+    PortData* data = (PortData*)userData;
     float* out = (float*)outputBuffer;
 
     (void)timeInfo;
@@ -95,53 +50,96 @@ static void CloseStream(PaStream* stream) {
     Pa_Terminate();
 }
 
-static void PortAudio(float pitch) {
-    PaStreamParameters outputParameters;
-    PaStream* stream;
-    PaError err;
-
-    paTestData data;
-    data.n = 0;
-    data.pitch = pitch;
-}
-
 static void DisplayImageWindow(Mat img) {
-    // Constantly refresh the board on a different thread, there is probably a better way of doing this.
+    // ConstantLY refresh the board on a different thread, there is probabLY a better way of doing this.
     while (true) {
         imshow("Image", img);
         waitKey(1);
     }
 }
 
-static imageStuff preprocessing() {
+static MandelData preprocessing() {
     Timer timer;
-    imageStuff image;
-    image.image = *new Mat(WIDTH, HEIGHT, CV_8UC3, Scalar(0, 0, 0));
-    for (int x = 0; x < WIDTH; x++) {
-        for (int y = 0; y < HEIGHT; y++) {
-            std::complex<float> point((float)x / WIDTH - 1.5, (float)y / HEIGHT - 0.5);
-            std::complex<float> z(0, 0);
-            int nb_iter = 0;
-            while (abs(z) < 2 && nb_iter <= 20) {
-                z = z * z + point;
-                nb_iter++;
+    MandelData image;
+    std::vector<uint8_t> escape_iter(LX * LY, MAX_ITER);
+
+    #pragma omp parallel for collapse(2)
+    for (size_t y = 0; y < LY; ++y) {
+        for (size_t bx = 0; bx < BLOCKS_X; ++bx) {
+            alignas(VECLEN * sizeof(bool)) bool finished[VECLEN] = { false };
+            alignas(VECLEN * sizeof(bool)) bool written[VECLEN] = { false };
+
+            alignas(VECLEN * sizeof(double)) double z_re[VECLEN] = { 0.0 };
+            alignas(VECLEN * sizeof(double)) double z_im[VECLEN] = { 0.0 };
+            alignas(VECLEN * sizeof(double)) double new_z_re[VECLEN];
+            alignas(VECLEN * sizeof(double)) double new_z_im[VECLEN];
+
+            alignas(VECLEN * sizeof(double)) double c_re[VECLEN] = { 0.0 };
+            for (size_t v = 0; v < VECLEN; ++v) {
+                c_re[v] = get_c_re(bx, v);
             }
-            if (nb_iter < 20) image.image.at<Vec3b>(Point(x, y)) = Vec3b(0, 0, 0);
-            else {
-                if (image.columnValue[x] == NULL) image.columnValue[x] = y;
-                image.image.at<Vec3b>(Point(x, y)) = Vec3b(255, 255, 255);
+            double c_im = get_c_im(y);
+
+            for (size_t iter = 0; iter < MAX_ITER; ++iter) {
+
+                #pragma omp simd aligned(z_re, z_im, new_z_re, new_z_im, finished)
+                for (size_t v = 0; v < VECLEN; ++v) {
+                    new_z_re[v] =
+                        z_re[v] * z_re[v] - z_im[v] * z_im[v] + c_re[v];
+                    new_z_im[v] = 2 * z_re[v] * z_im[v] + c_im;
+
+                    z_re[v] = new_z_re[v];
+                    z_im[v] = new_z_im[v];
+
+                    auto const abs = z_re[v] * z_re[v] + z_im[v] * z_im[v];
+
+                    if (!finished[v] && (abs > RADIUS)) {
+                        finished[v] = true;
+                        escape_iter[idx(bx, y) * VECLEN + v] = iter;
+                    }
+                }
+
+                bool all_finished = true;
+                for (size_t v = 0; v < VECLEN; ++v) {
+                    all_finished &= finished[v];
+                }
+
+                if (all_finished) {
+                    break;
+                }
+            }
+        }
+    }
+
+    image.canvas = *new cv::Mat(LY, LX, CV_8UC1, Scalar(0, 0, 0));
+    for (size_t bx = 0; bx < BLOCKS_X; bx++)
+    {
+        bool top_pixel_found = false;
+        for (size_t y = 0; y < LY; y++){
+        
+            for (size_t v = 0; v < VECLEN; v++)
+            {
+                uint8_t transformed = transform(escape_iter[idx(bx, y) * VECLEN + v], MAX_ITER, 255);
+                int x = bx * VECLEN + v;
+                if (transformed == 255) {
+                    if (!top_pixel_found){
+                        image.topdownPixelView.push_back(*new PixelCoordinates{ x,static_cast<int>(y) });
+                        top_pixel_found = true;
+                    }
+                }
+                image.canvas.at<uint8_t>(y, x) = transformed;
             }
         }
     }
     return image;
 }
 
-static void restShit(imageStuff img) {
+static void PlayAudio(MandelData img) {
     PaStreamParameters outputParameters;
     PaStream* stream;
     PaError err;
 
-    paTestData data;
+    PortData data;
     data.n = 0;
 
     err = Pa_Initialize();
@@ -171,23 +169,26 @@ static void restShit(imageStuff img) {
     err = Pa_SetStreamFinishedCallback(stream, &StreamFinished);
     if (err != paNoError) ErrorExit("Stream finished callback failed | %i", err);
 
-    float splitter = 10.f / img.image.rows;
-    for (size_t i = 0; i < WIDTH; i++)
+    float splitter = 10.f / img.canvas.rows;
+
+
+    for (auto& e : img.topdownPixelView)
     {
-        rectangle(img.image, Point(i - 5, img.columnValue[i] - 5), Point(i, img.columnValue[i]), Scalar(255, 255, 255));
-        data.pitch = splitter * img.columnValue[i];
+        rectangle(img.canvas, Point(e.x - 10, e.y - 10), Point(e.x+10, e.y+10), Scalar(255, 255, 255));
+        data.pitch = splitter * e.y;
         PlaySound(stream);
     }
+ 
     CloseStream(stream);
 }
 
 int main() {
     printf("Generating mandelbrot, this can take a while...\n");
-    imageStuff img = preprocessing();
-    std::thread t1(DisplayImageWindow, img.image);
-    std::thread t2(restShit, img);
-    t1.join();
-    t2.join();
+    MandelData img = preprocessing();
+    std::thread ImageDisplayThread(DisplayImageWindow, img.canvas);
+    std::thread PlayAudioThread(PlayAudio, img);
+    ImageDisplayThread.join();
+    PlayAudioThread.join();
     
 	return 0;
 }
